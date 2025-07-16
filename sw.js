e5f34f18831dcddb39c06a606a615acb3c49b3b3
FahimCreatior <153365037+FahimCreatior@users.noreply.github.com>
2025-07-16 12:11:17 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,75 @@
+const CACHE_NAME = 'faheemz-tech-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/mainpage.html',
+  '/movies.html',
+  '/movies2.html',
+  '/football.html',
+  '/music.html',
+  'https://i.ibb.co/GfZqhFx8/FAHEEMZTECH.jpg',
+  'https://cdn.glitch.global/a7498174-0fcc-4e40-8eaa-4769d7d5a587/WhatsApp%20Image%202024-12-12%20at%2010.25.02%20AM.jpeg?v=1733993135965',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap'
+];
+
+// Install event - cache all static assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Activate event - clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch event - serve from cache, falling back to network
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // Clone the request
+        const fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          (response) => {
+            // Check if we received a valid response
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // Clone the response
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then((cache) => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
